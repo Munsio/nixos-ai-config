63acc4f1e92a83b1305e80d140a9eede24b588e5
Martin Treml <git@treml.dev>
2025-05-06 14:26:28 +0200

    Add extraLocale to hostVars and configure i18n.extraLocaleSettings

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, hostVars, ... }:
+{ lib, hostVars, ... }:
 
 {
   # Common configuration for all hosts
@@ -13,13 +13,23 @@
   time.timeZone = lib.mkDefault hostVars.timezone;
 
   # Select internationalisation properties
-  i18n.defaultLocale = lib.mkDefault hostVars.locale;
+  i18n = {
+    defaultLocale = lib.mkDefault hostVars.locale;
+    extraLocaleSettings = lib.mkDefault {
+      LC_NUMERIC = hostVars.extraLocale;
+      LC_TIME = hostVars.extraLocale;
+      LC_MONETARY = hostVars.extraLocale;
+      LC_ADDRESS = hostVars.extraLocale;
+      LC_IDENTIFICATION = hostVars.extraLocale;
+      LC_MEASUREMENT = hostVars.extraLocale;
+      LC_PAPER = hostVars.extraLocale;
+      LC_TELEPHONE = hostVars.extraLocale;
+      LC_NAME = hostVars.extraLocale;
+    };
+  };
 
   # Configure console keymap
   console.keyMap = lib.mkDefault hostVars.keyboardLayout;
-
-  # Basic packages for all systems
-  environment.systemPackages = with pkgs; [ vim wget git curl ];
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software.
--- hosts/example/variables.nix
+++ hosts/example/variables.nix
@@ -1,7 +1,10 @@
 {
-  # Host-specific variables for the 'example' host
+  # Locale and Charset configuration
   keyboardLayout = "us";
   timezone = "UTC";
   locale = "en_US.UTF-8";
+  extraLocale = "en_US.UTF-8";
+
+  # Nix and HomeManager stateVersion
   stateVersion = "24.11";
 }
